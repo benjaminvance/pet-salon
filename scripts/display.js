a12100d82660468190da283c19b638e27cbbb827
Benjamin Vance <53717770+benjaminvance@users.noreply.github.com>
2022-04-25 19:08:31 -0500

    Competency report
    Added new element and updated html pages

--- scripts/display.js
+++ scripts/display.js
@@ -7,44 +7,48 @@ function displayPet(){
         console.log(tmp);
     }
     document.getElementById("pets").innerHTML=tmp;
-    innerHTML=tmp;
+    // innerHTML=tmp;
 }
-//display the pets in cards
-// function displayCards(){
-//     let tmp="";
-//     for(let i=0;i<petSalon.pets.length;i++){
-//         let pet = petSalon.pets[i];
-//         tmp=`
-//         <div class="pet">
-//             <h4>${pet.name}</h4>
-//             <label>${pet.age}</label>
-//             <label>${pet.gender}</label>
-//             <label>${pet.breed}</label>
-//             <label>${pet.service}</label>
-//             <label>${pet.ownerName}</label>
-//             <label>${pet.contactPhone}</label>
-//         `;
-//     }
-//     document.getElementById("pets").innerHTML=tmp;
-// }
-
-
-//display the pets in table
-
+// display the pets in cards
 function displayCards(){
     let tmp="";
     for(let i=0;i<petSalon.pets.length;i++){
         let pet = petSalon.pets[i];
-        tmp=`
-        <div class="pet">
-            <h3>${pet.name}</h3>
+        tmp+=`
+        <div id="${pet.id}" class="pet">
+            <div class="pet-header">
+                <h4>${pet.name}</h4>
+                <button onclick="deletePet(${pet.id});">🗑️</button>
+            </div>
             <p>${pet.age}</p>
             <p>${pet.gender}</p>
             <p>${pet.breed}</p>
             <p>${pet.service}</p>
             <p>${pet.ownerName}</p>
             <p>${pet.contactPhone}</p>
+            <p>${pet.comments}</p>
+            </div>
         `;
     }
     document.getElementById("pets").innerHTML=tmp;
 }
+
+
+//display the pets in table
+// function displayCards(){
+//     let tmp="";
+//     for(let i=0;i<petSalon.pets.length;i++){
+//         let pet = petSalon.pets[i];
+//         tmp=`
+//         <div class="pet">
+//             <h3>${pet.name}</h3>
+//             <p>${pet.age}</p>
+//             <p>${pet.gender}</p>
+//             <p>${pet.breed}</p>
+//             <p>${pet.service}</p>
+//             <p>${pet.ownerName}</p>
+//             <p>${pet.contactPhone}</p>
+//         `;
+//     }
+//     document.getElementById("pets").innerHTML=tmp;
+// }
--- scripts/register.js
+++ scripts/register.js
@@ -12,7 +12,9 @@ let petSalon = {
     pets:[]
     }
 
-function Pet(name,age,gender,breed,service,ownerName,contactPhone){
+let c=0;
+
+function Pet(name,age,gender,breed,service,ownerName,contactPhone,comments){
     this.name=name;
     this.age=age;
     this.gender=gender;
@@ -20,6 +22,8 @@ function Pet(name,age,gender,breed,service,ownerName,contactPhone){
     this.service=service;
     this.ownerName=ownerName;
     this.contactPhone=contactPhone;
+    this.comments=comments;
+    this.id=c++; //increase the var 1 evertime that it is used
 }
 
 //get the info from the inputs and store the info
@@ -30,6 +34,7 @@ let inputBreed=document.getElementById("txtBreed");
 let inputService=document.getElementById("selService");
 let inputOwner=document.getElementById("txtOwner");
 let inputPhone=document.getElementById("txtPhone");
+let inputComments=document.getElementById("txtComments");
 
 function isValid(aPet){
     //return false value when the pet is not valid
@@ -40,20 +45,18 @@ function isValid(aPet){
         valid=false;
         console.log("Invalid name");
     }
-    
-    return valid;
+        return valid;
 }
 
 function register(){
     //create the pet
-    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputOwner.value,inputPhone.value);
-    if(isValid(thePet)==true){
+    let thePet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value,inputOwner.value,inputPhone.value,inputComments.value);
+    if(isValid(thePet)){
     //push the pet info into array
     petSalon.pets.push(thePet);
     displayCards();
     clearInputs();
     }
-    
 }
 
 function clearInputs(){
@@ -61,24 +64,58 @@ function clearInputs(){
     inputAge.value="";
     inputBreed.value="";
     inputGender.value="";
-    inputService.value="";
+    // inputService.value="";
     inputOwner.value="";
     inputPhone.value="";
+    inputComments.value="";
+}
+
+function deletePet(petId){
+    console.log("deleting..." + petId);
+    let deleteIndex;
+    //previous actions---
+        //we need an id in the pet constr
+        //we need a delete button in the html
+    //in this function---
+        //travel the array (for loop)
+        for(let i=0;i<petSalon.pets.length;i++){
+            let pet = petSalon.pets[i];
+            if(petId==pet.id){
+                //find the id (if)
+                //get the position in the array (store in var)
+                deleteIndex=i;
+                console.log("Found in position: " + i);
+            }
+        }
+        //remove from the array (splice)
+        petSalon.pets.splice(deleteIndex,1);
+        //remove from the html (remove)
+        document.getElementById(petId).remove();
+        //display a message to the user
+}
+
+function searchPet(){
+    console.log("Searching");
+
+    let searchString = document.getElementById("txtSearch").value;
+    console.log("Searching " + searchString);
+
+    for(let i=0;i<petSalon.pets.length;i++){
+        let pet = petSalon.pets[i];
+
+        if(searchString === pet.name.toLowerCase()){
+            alert("I found it");
+            document.getElementById(pet.id).classList.add("highlight");
+        }else{
+            document.getElementById(pet.id).classList.remove("highlight");
+        }
+    }
 }
 
 let Shaggy= new Pet("Shaggy",3,"Male","Labrador","Grooming","Ben","6365281245");
 let Marley= new Pet("Marley",5,"Female","Golden Retriever","Nails Cutting","Dave","3144625099");
 let Buddy= new Pet("Buddy",4,"Female","Pitbull","Teeth cleaning","Zach","6606806060");
 petSalon.pets.push(Shaggy,Marley,Buddy);
-
 console.log(Shaggy,Marley,Buddy);
-
 console.log(petSalon);
 
-function displaySalonInfo(){
-
-}
-
-function displayPetsNames(){
-    
-}
